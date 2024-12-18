a47edb2425827d44bee2d1489380c4870122857b
zhanghm36 <hmzhang_bioinf@163.com>
2024-12-18 16:24:16 +0800

    Update work.sh

--- circTargetScan/remove_corrordantly_mapped_reads/work.sh
+++ circTargetScan/remove_corrordantly_mapped_reads/work.sh
@@ -1,7 +1,10 @@
+#Map reads to the genome using HISAT2
 hisat2 -x $genome_ref_fasta -U /read1.fq -S /read1.toGenome.hisat2.sam --un  --no-softclip --phred33 -p 8 2> /read1.toGenome.hisat2.log
 hisat2 -x $genome_ref_fasta -U /read2.fq -S /read2.toGenome.hisat2.sam --un  --no-softclip --phred33 -p 8 2> /read2.toGenome.hisat2.log
 
-#get the not corrordantly mapped read1 and read2
+#Discard concordantly mapped Read 1 and Read 2 using HISAT2
 perl remove_both_mapping_reads.pl  read1.fq  read2.fq  read1.toGenome.hisat2.sam  read2.toGenome.hisat2.sam  read1.toGenome.unmapped.any.byHisat2.fq  read2.toGenome.unmapped.any.byHisat2.fq
+
+#Remap unmapped reads to the genome using BWA, which supports for chimeric alignment
 bwa mem -t 16 -k 12 -T 15 -o /read1.toGenome.bwa.sam $genome_ref_fasta /read1.toGenome.unmapped.any.byHisat2.fq
 bwa mem -t 16 -k 12 -T 15 -o /read2.toGenome.bwa.sam $genome_ref_fasta /read2.toGenome.unmapped.any.byHisat2.fq
